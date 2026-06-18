b2021d890e3a6e7a66e8136b9fb04580ee636b89
Himanshu3607 <himans2713@gmail.com>
2026-06-18 12:24:08 +0530

    Complete calculator functionality

--- src/Calculator.java
+++ src/Calculator.java
@@ -16,6 +16,22 @@ public class Calculator {
     double modulus(double x, double y){
         return x % y;
     }
+    double power(double x, double y){
+        return Math.pow(x, y);
+    }
+    double sqRoot(double x){
+        if(x<0){
+            System.out.println("Square root of negative number is not possible.");
+            return 0;
+        }
+        return Math.sqrt(x);
+    }
+    double maximum(double x, double y){
+        return Math.max(x, y);
+    }
+    double minimum(double x, double y){
+        return Math.min(x, y);
+    }
 
     public static void main(String args[]) {
 
@@ -30,17 +46,21 @@ public class Calculator {
             System.out.println("3. Multiplication");
             System.out.println("4. Division");
             System.out.println("5. Modulus");
-            System.out.println("6. Exit");
+            System.out.println("6. Power");
+            System.out.println("7. Square Root");
+            System.out.println("8. Maximum");
+            System.out.println("9. Minimum");
+            System.out.println("10. Exit");
 
             System.out.print("Enter choice: ");
             int ch = sc.nextInt();
 
-            if (ch == 6) {
+            if (ch == 10) {
                 System.out.println("Exiting...");
                 break;
             }
 
-            if (ch < 1 || ch > 6) {
+            if (ch < 1 || ch > 10) {
                 System.out.println("Invalid Choice!");
                 continue;
             }
@@ -48,8 +68,12 @@ public class Calculator {
             System.out.print("Enter x: ");
             double x = sc.nextDouble();
 
-            System.out.print("Enter y: ");
-            double y = sc.nextDouble();
+            double y = 0;
+
+            if(ch != 7){
+                System.out.print("Enter y: ");
+                y = sc.nextDouble();
+            }
 
             switch (ch) {
                 case 1:
@@ -72,7 +96,19 @@ public class Calculator {
                     }
                     break;
                 case 5: 
-                    System.out.println("Result= " + calc.modulus(x, y));
+                    System.out.println("Result = " + calc.modulus(x, y));
+                    break;
+                case 6: 
+                    System.out.println("Result = "+ calc.power(x, y));
+                    break;
+                case 7:
+                    System.out.println("Result = "+ calc.sqRoot(x));
+                    break;
+                case 8:
+                    System.out.println("Result = "+ calc.maximum(x, y));
+                    break;
+                case 9:
+                    System.out.println("Result = "+calc.minimum(x, y));
                     break;
             }
         }
